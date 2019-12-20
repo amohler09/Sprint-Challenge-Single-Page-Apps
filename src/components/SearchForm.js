ad2ed230f8d593690939170096e8a1d2a3702276
Amber Mohler <ambermohler@imac.lan>
2019-12-20 12:03:03 -0500

    made very basic outline of search formm, needs styling

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,50 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 
 export default function SearchForm() {
+  const [search, setSearch] = useState('');
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`https://rickandmortyapi.com/api/character/`)
+      .then(response => {
+        const characters = response.data.results.filter(
+          character =>
+          character.name.toLowerCase().includes(search.toLowerCase())
+        );
+         console.log(characters);
+         setData(characters);
+      })
+      .catch(error => console.log('error on search', error))
+  },[search] )
+
+    const handleChange = event => {setSearch(event.target.value)};
  
   return (
     <section className="search-form">
-     // Add a search form here
+        <form>
+          <input 
+            type='text' 
+            placeholder='Search'
+            onChange={handleChange}
+            value={search}
+            name='search' />
+        </form>
+
+        <div>
+          {data.map(item => {
+            return (
+            <div>
+              <img src={item.image} />
+              <p>Name: {item.name}</p>
+              <p>Currently {item.status}</p>
+              <p>Identifies as {item.gender}</p>
+
+              
+              </div>
+          )})}
+        </div>
     </section>
   );
 }
--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -1,15 +1,19 @@
 import React from "react";
+import { Route } from 'react-router-dom';
+
 
 export default function WelcomePage() {
   return (
     <section className="welcome-page">
       <header>
         <h1>Welcome to the ultimate fan site!</h1>
+        <Route exact path='/'>
         <img
           className="main-img"
           src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"
           alt="rick"
         />
+        </Route>
       </header>
     </section>
   );

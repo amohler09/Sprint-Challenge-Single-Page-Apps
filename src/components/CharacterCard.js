ea86d0521e374b00813c2dc1c6bb789eaa0cf824
Amber Mohler <ambermohler@imac.lan>
2019-12-20 11:26:22 -0500

    completed part one of sprint, rendered characters to the screen from api

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,16 @@
 import React from "react";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+
+export default function CharacterCard(props) {
+
+
+  return (
+    <div>
+      <img src={props.details.image} alt='rick and morty character' />
+      <p>Name:{props.details.name}</p>
+      <p>{props.details.status}</p>
+      <p>{props.details.species}</p>
+      <p>{props.details.gender}</p>
+    </div>
+  );
 }
